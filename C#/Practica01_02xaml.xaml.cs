2962f18d47535ccff821772a99aad59a6bbb02fe
Cesar Olvera <a2233336167@alumnos.uat.edu.mx>
2024-09-28 11:25:56 -0600

    Add files via upload

---
+++ C#/Practica01_02xaml.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace PracticasUnidad1
+{
+    public partial class Practica01_02xaml : Window
+    {
+        public Practica01_02xaml()
+        {
+            InitializeComponent();
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            Practica01_02xaml window = new Practica01_02xaml();  // cambia Practica01_02 por Practica01_02xaml
+            app.Run(window);
+        }
+    }
+}
---
+++ C#/Practica01_03.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PracticasUnidad1
+{
+    /// <summary>
+    /// Lógica de interacción para Practica01_03.xaml
+    /// </summary>
+    public partial class Practica01_03 : Window
+    {
+        public Practica01_03()
+        {
+            InitializeComponent();
+        }
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            Practica01_03 dialog = new Practica01_03();
+            dialog.ShowDialog(); // Se asegura de que sea modal, como el JDialog en Java
+        }
+    }
+}
